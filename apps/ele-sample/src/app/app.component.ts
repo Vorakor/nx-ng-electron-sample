652e2ff6ae4f8b1f810b1d51dbb431962143472a
Shaine Berube <sberube@cricut.com>
2022-04-07 12:54:43 -0600

    initial commit

---
+++ apps/ele-sample/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ng-electron-sample-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ele-sample';
+}
